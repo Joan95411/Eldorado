dc9c471fe639fd748dcc1c69e137c4d8a9854a36
B.abbink <info@basabbink.nl>
2024-05-08 12:15:36 +0200

    Updated pipeline once again

--- src/test/java/org/set/TestMainImplemented.java
+++ src/test/java/org/set/TestMainImplemented.java
@@ -2,7 +2,7 @@ package org.set;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class TestMain {
+public class TestMainImplemented {
 
     @Test
     public void testGetLucky() {
