4c1789aaccb96c9cccfa9cc92db5195752b03f78
B.abbink <info@basabbink.nl>
2024-05-13 20:26:51 +0200

    Added test for the class HexagonGameBoard

---
+++ src/test/java/org/set/HexagonGameBoardTest.java
@@ -0,0 +1,91 @@
+package org.set;
+
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HexagonGameBoardTest {
+    public HexagonGameBoard hexagonGameBoard = new HexagonGameBoard(5, 5, 50);
+
+    @Test
+    public void testHexagonGameBoard() {
+        assertNotNull(hexagonGameBoard);
+        assertNotNull(hexagonGameBoard.getPreferredSize());
+
+        assertEquals(hexagonGameBoard.numRows, 5);
+        assertEquals(hexagonGameBoard.numCols, 5);
+        assertEquals(hexagonGameBoard.hexSize, 50);
+    }
+
+    @Test
+    public void testHexagonGameBoardLocation() {
+        assertEquals(hexagonGameBoard.getLocation(), new Point(0,0));
+        hexagonGameBoard.setLocation(new Point(1,1));
+        assertEquals(hexagonGameBoard.getLocation(), new Point(1,1));
+    }
+
+    @Test
+    public void testHexagonGameBoardPlayer() {
+        Color[] colors = {Color.RED, Color.GREEN, Color.BLUE, Color.YELLOW};
+        int row = 0;
+        int col = 0;
+        int maxPlayers = 4;
+
+//        TODO: This worked but is there a nicer way?
+        BufferedImage image = new BufferedImage(20, 20, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2d = image.createGraphics();
+
+        hexagonGameBoard.paintComponent(g2d);
+
+        for (int i = 0; i < maxPlayers; i++) {
+//            Add a new player
+            hexagonGameBoard.players.add(new Player(colors[i]));
+            assertEquals(hexagonGameBoard.players.size(), i + 1);
+
+            int playerId = hexagonGameBoard.players.size() - 1;
+
+//            Check the id and color of the player
+            assertEquals(hexagonGameBoard.players.get(playerId).id, hexagonGameBoard.players.size());
+            assertEquals(hexagonGameBoard.players.get(playerId).color, colors[i]);
+
+//            Set the players position and check if it actually does set the position
+            hexagonGameBoard.players.get(playerId).setPlayerPosition(row + i, col + i);
+
+            assertEquals(hexagonGameBoard.players.get(playerId).getCurrentCol(), col + i);
+            assertEquals(hexagonGameBoard.players.get(playerId).getCurrentRow(), row + i);
+        }
+
+//        Check if the players are added
+        assertTrue(hexagonGameBoard.players.size() <= maxPlayers);
+
+//        TODO: This should fail because the max amount of players is reached
+        hexagonGameBoard.players.add(new Player(colors[0]));
+    }
+
+    private void addPlayer(int index, int row, int col, Color color) {
+
+    }
+
+    @Test
+    public void testHexagonLoadTileData() throws IOException {
+        JSONObject tileInfo = null;
+
+        HexagonGameBoard hexagonGameBoard = new HexagonGameBoard(5, 5, 50);
+
+        String tileDataPath = "/Users/basabbink/Development/set2024team04project/src/main/java/org/set/tileData.json";
+        String tileDataJson = new String(Files.readAllBytes(new File(tileDataPath).toPath()));
+        JSONObject tileData = new JSONObject(tileDataJson);
+        tileInfo = tileData.getJSONObject("tiles");
+
+        assertNotNull(tileInfo);
+        assertNotNull(hexagonGameBoard.tileInfo);
+        assertEquals(hexagonGameBoard.tileInfo.toString(), tileInfo.toString());
+    }
+}
