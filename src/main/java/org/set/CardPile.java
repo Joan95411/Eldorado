d07ee41d89280ab700bd6cbcd2f244e3e5c5a0f7
basva <b.m.vandeweerd@student.utwente.nl>
2024-05-22 20:02:52 +0200

    Implemented CardPile, all tests passed

--- src/main/java/org/set/CardPile.java
+++ src/main/java/org/set/CardPile.java
@@ -1,41 +1,45 @@
 package org.set;
 
+import java.util.Collections;
 import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
-import org.set.Card;
 
 public class CardPile {
-    private LinkedList<Card> drawPile;
-    private LinkedList<Card> discardPile;
-    public Exception NoCardsToDrawException = new Exception("The drawPile and discardPile are empty, can't draw any cards");
+    private final LinkedList<Card> drawPile;
+    private final LinkedList<Card> discardPile;
 
-
-
-
-    public CardPile(){
+    public CardPile() {
+        drawPile = new LinkedList<>();
+        discardPile = new LinkedList<>();
     }
 
     //shuffle the discard pile to create a new pile
-    private void shuffle(){
-
+    private void shuffle() {
+        drawPile.addAll(discardPile);
+        discardPile.clear();
+        Collections.shuffle(drawPile);
     }
 
     //Add a card to the discard pile
-    public void discard(Card card){
-
+    public void discard(Card card) {
+        discardPile.add(card);
     }
 
     //return the top card of the drawPile, if it is empty shuffle discardPile to create new drawPile
-    public Card draw() throws Exception{
-        return null;
+    public Card draw() throws Exception {
+        if (drawPile.isEmpty()) {
+            if (discardPile.isEmpty()) {
+                throw new Exception("No cards to draw in discard pile.");
+            }
+            shuffle();
+        }
+        return drawPile.remove();
     }
 
-    public LinkedList<Card> getDrawPile(){
+    public LinkedList<Card> getDrawPile() {
         return drawPile;
     }
 
-    public LinkedList<Card> getDiscardPile(){
+    public LinkedList<Card> getDiscardPile() {
         return discardPile;
     }
 
