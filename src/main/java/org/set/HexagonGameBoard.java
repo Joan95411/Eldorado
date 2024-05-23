a9881b2f3e3ba4e1554a1432983daf7f7e8d9c86
x.zhao-5 <x.zhao-5@student.utwente.nl>
2024-05-23 11:31:59 +0200

    test commit

--- src/main/java/org/set/HexagonGameBoard.java
+++ src/main/java/org/set/HexagonGameBoard.java
@@ -104,7 +104,7 @@ public class HexagonGameBoard extends JPanel  {
                     currentTileInfo.put("points", 0);
                 }
                 String colorName = currentTileInfo.getString("color");
-                Color color = getColorFromString(colorName);
+                Color color = Util.getColorFromString(colorName);
                 int points = currentTileInfo.getInt("points");
                 tile.setColor(color);
                 tile.setPoints(points);
@@ -170,18 +170,7 @@ public class HexagonGameBoard extends JPanel  {
                 .collect(Collectors.toList());
     }
     
-    public Color getColorFromString(String colorName) {
-        Map<String, Color> colorMap = new HashMap<>();
-        colorMap.put("gray", Color.GRAY);
-        colorMap.put("red", Color.RED);
-        colorMap.put("yellow", Color.YELLOW);
-        colorMap.put("green", Color.GREEN);
-        colorMap.put("blue", Color.BLUE);
-        colorMap.put("pink", Color.PINK);
-        colorMap.put("black", Color.BLACK);
-        colorMap.put("cyan", Color.CYAN);
-        return colorMap.getOrDefault(colorName.toLowerCase(), Color.WHITE);
-    }
+    
     
     public void addTerrain(int addRow, int addCol,Terrain terrainA){
     	List<WinningPiece> WP=getAllWinningPieces();
