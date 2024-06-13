1c33e5853db248d01c8dac597746a0274a2d6dbb
Abbink, B.B.L. (Bas, Student M-ITECH) <b.b.l.abbink@student.utwente.nl>
2024-06-13 15:29:47 +0200

    Refactored: Fixed 'Strings and Boxed types should be compared using "equals()"'

--- src/main/java/org/set/cards/Card.java
+++ src/main/java/org/set/cards/Card.java
@@ -34,7 +34,7 @@ public abstract class Card {
 			return CardType.BLUE;
 		} else if (Arrays.asList(yellowCardTypes).contains(this.name)) {
 			return CardType.YELLOW;
-		} else if (Arrays.asList(jokerCardTypes).contains(this.name) || this.name == "Native") {
+		} else if (Arrays.asList(jokerCardTypes).contains(this.name) || this.name.equals("Native")) {
 			return CardType.JOKER;
 		} else if (Arrays.asList(purpleCardTypes).contains(this.name)) {
 			return CardType.PURPLE;
