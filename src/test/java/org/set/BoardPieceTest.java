5f7e40b619d4c855fac35df1cb1375543976aaa1
Joan95411 <x.zhao-5@student.utwente.nl>
2024-06-21 16:22:52 +0200

    111

--- src/test/java/org/set/BoardPieceTest.java
+++ src/test/java/org/set/BoardPieceTest.java
@@ -2,15 +2,33 @@ package org.set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.awt.Color;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.set.player.Player;
 import org.set.template.Team03Board;
+import org.set.template.Template;
 
 class BoardPieceTest {
 
-	@Test
-	void test() {
-		Team03Board Board = new Team03Board(35, 35, 25);
-		 Board.boardPieces.clear();
-	}
+	private Template board;
+    private static InputStream backupInputStream;
+    
+    @BeforeAll
+    public static void start() {
+       
+        backupInputStream = System.in;
+    }
+    @BeforeEach
+    public void setUp() {
+    	board = new Team03Board(35, 35, 25); 
+        Random random = new Random();
+    }
 
 }
